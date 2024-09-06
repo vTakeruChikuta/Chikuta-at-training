a4495a550b705fad43ce4309837c0bfaf0917cc3
vTakeruChikuta <takeru.chikuta@hd.veriserve.co.jp>
2024-09-06 12:28:31 +0900

    「文字列と入出力」の課題です

---
+++ chapter2_calc_01.py
@@ -0,0 +1,10 @@
+hight_1 = 180
+hight_2 = 175
+hight_3 = 160
+hight_4 = 165
+hight_5 = 175
+mi = min(hight_1,hight_2,hight_3,hight_4,hight_5)
+ma = max(hight_1,hight_2,hight_3,hight_4,hight_5)
+total = hight_1 + hight_2 + hight_3 + hight_4 + hight_5
+
+print('5人の身長の最小値は',mi,'cm、最大値は',ma,'cm、合計値は',total,'cm')
